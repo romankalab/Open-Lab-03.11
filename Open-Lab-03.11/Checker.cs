dea45168a2dbf5e30b3bb357db7fc22c701afa48
romankalab <roan.kalab@mail.com>
2019-10-31 16:37:51 +0100

    Initial

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            string firsthalf = str.Substring(0, str.Length / 2);
+            char[] array = str.ToCharArray();
+
+            Array.Reverse(array);
+
+            string reversedstr = new string(array);
+            string secondhalf = reversedstr.Substring(0, reversedstr.Length / 2);
+
+            return firsthalf == secondhalf;
         } 
     }
 }
